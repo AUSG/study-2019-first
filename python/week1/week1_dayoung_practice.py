3c60833cb756c2a95b5e5aa84fe0959a2e0011e5
dyyoon <dayoung927@gmail.com>
2019-02-19 21:11:28 +0900

    week1_dayoung_practice
    연습문제

---
+++ python/week1/week1_dayoung_practice.py
@@ -0,0 +1,29 @@
+# python_firstweek_practice
+
+
+#practice1
+a = "Hello, World!"
+print(a)
+
+
+
+#practice2_1
+b = 2
+print(b*1)
+print(b*2)
+print(b*3)
+print(b*4)
+print(b*5)
+#practice2_2_구구단
+for c in range (1,6) :
+    print("2 * %d =" % c, b*c)
+
+
+
+
+
+#practice3
+age = 24
+print('제 나이는 만',age,'살입니다.')
+
+
