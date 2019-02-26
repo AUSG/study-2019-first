4468d44ac2b1c71161e8a3ea91c8df12660898ee
Ji Yoon <certi98@naver.com>
2019-02-26 13:44:33 +0900

    python_week1_practice_jiyoon

---
+++ python/week1/week1_practice_jiyoon.py
@@ -0,0 +1,18 @@
+#practice_1
+
+a = "Hello, World!"
+print(a)
+
+#practice_2
+
+b = 2
+print(b*1)
+print(b*2)
+print(b*3)
+print(b*4)
+print(b*5)
+
+#practice_3
+
+age = 21
+print("제 나이는 만", age, "살입니다")
